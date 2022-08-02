fbb1b7c90ce3d250f50dd64b5796047e9d7a9110
袁帅 <17602230056@163.com>
2022-08-02 15:05:10 +0800

    Initial commit

---
+++ super-cloud-system/src/main/java/com/superCloud/SuperCloudSystemApplication.java
@@ -0,0 +1,19 @@
+package com.superCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统管理
+ * @author ys
+ * @version 1.0
+ * @date 2022/8/2 11:28
+ */
+@SpringBootApplication
+public class SuperCloudSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCloudSystemApplication.class,args);
+    }
+
+}
